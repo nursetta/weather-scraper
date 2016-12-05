ed06edb3b28fac3037015355377ab4541df469fa
nursetta <n_ursetta@yahoo.com>
2016-12-05 12:49:25 -0700

    added php.ini, error handlers, and allowed user to find any city

--- index.php
+++ index.php
@@ -1,13 +1,35 @@
 <?php
 
-if ($_GET["city"]) {
-    $forecastPage = file_get_contents("http://www.weather-forecast.com/locations/London/forecasts/latest");
+$weather = "";
+$error = "";
+
+if (array_key_exists('city', $_GET)) {
+    
+    $city = str_replace(' ', '', $_GET['city']);
+    
+    $file_headers = @get_headers("http://www.weather-forecast.com/locations/".$city."/forecasts/latest");
+    
+    if($file_headers[0] == 'HTTP/1.1 404 Not Found') {
+        $error = "That City could not be found";
+    } else {
+    
+    $forecastPage = file_get_contents("http://www.weather-forecast.com/locations/".$city."/forecasts/latest");
     
     $pageArray = explode('3 Day Weather Forecast Summary:</b><span class="read-more-small"><span class="read-more-content"> <span class="phrase">', $forecastPage);
+        
+    if (sizeof($pageArray) > 1) {
     
-    $secondPageArray = explode('</span></span></span>', $pageArray[1]);
-    
-    $weather = $secondPageArray[0];
+        $secondPageArray = explode('</span></span></span>', $pageArray[1]);
+        
+        if (sizeof($secondPageArray) > 1) {
+            $weather = $secondPageArray[0];    
+        } else {
+            $error = "Sorry, we can't seem to get the weather for that city. Please try again later.";
+        }
+    } else {
+        $error = "Sorry, we can't seem to get the weather for that city. Please try again later.";
+        }
+    }
 }
 
 ?>
@@ -37,7 +59,11 @@ if ($_GET["city"]) {
             <!-- Weather Input   -->
             <fieldset class="form-group">
                 <label for="city">Enter the name of a city</label>
-                <input type="text" class="form-control" id="city" name="city" placeholder="Eg. Denver, Seattle">
+                <input type="text" class="form-control" id="city" name="city" placeholder="Eg. Denver, Seattle" value="<?php
+                    if (array_key_exists('city', $_GET)) {
+                        echo $_GET['city'];
+                    }
+                ?>">
             </fieldset>
 
             <button type="submit" id="submit" class="btn btn-primary">Submit</button>
@@ -46,7 +72,9 @@ if ($_GET["city"]) {
         <div id="weather"><?php
             if ($weather) {
                 echo '<div class="alert alert-success" role="alert">'.$weather.'</div>';
-            }    
+            } else if ($error) {
+                echo '<div class="alert alert-danger" role="alert">'.$error.'</div>'; 
+            }
         ?></div>
     
     </div>
